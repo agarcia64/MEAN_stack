fe92d0c8cba2b94cb3694d00460b69c4aab318ab
Alfredo Garcia <suitcase@bu.edu>
2019-06-05 14:49:30 -0400

    hard reset

--- routes/ps4.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET home page. */
-router.get('/ps4', function(req, res, next) {
-    res.render('ps4', { displayText: 'UwU' });
-});
-
-module.exports = router;
